f81d02a0a9995575d7c50acc46d738948761a1c8
yanboit <72846735+yanboit@users.noreply.github.com>
2024-12-06 06:20:27 +0800

    Update config.prod.ts

--- docker/config.prod.ts
+++ docker/config.prod.ts
@@ -1,5 +1,5 @@
 const config: IConfig = {
-  serve: `http://${window.location.hostname}:38080`,
+  serve: `http://1.13.23.200:38080`,
   keys: ['some secret hurr'],
   session: {
     key: 'koa:sess',
